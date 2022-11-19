f3c9b32c5acc638bf827d57c926a614465197b1c
Joshua <josuealexander1215@gmail.com>
2022-11-18 22:46:58 -0600

    testing the *ngFor directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-udemy';
+
+  listaDeCursos:String[] = ["Javascrip", "Java", "Python", "API Testing", "Automation"];
+  constructor() { 
+    testing();
+  } 
+
 }
+
+function testing(){
+  let test:string[] = ["a", "b", "c"];
+
+  for(let t of test){
+    console.log(t);
+  }
+}
+
