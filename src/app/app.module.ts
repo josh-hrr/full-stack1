5af56f97cbbd441e34f86319f3d316b36658cb1e
Joshua <josuealexander1215@gmail.com>
2022-11-25 23:26:32 -0600

    adding bootstrap and "directivas" component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { ClientesComponent } from './clientes/clientes.component';
+import { DirectivasComponent } from './directivas/directivas.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    ClientesComponent
+    ClientesComponent,
+    DirectivasComponent
   ],
   imports: [
     BrowserModule,
