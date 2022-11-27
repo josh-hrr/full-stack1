9286d74cddd8f8d77caff54af17383b3050ef4ee
Joshua <josuealexander1215@gmail.com>
2022-11-26 21:25:58 -0600

    adding angular router to header links

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ClientesComponent } from './clientes/clientes.component';
+import { DirectivasComponent } from './directivas/directivas.component';
 
 const routes: Routes = [
-  {path: 'clientes', component: ClientesComponent}  
+  { path: 'clientes', component: ClientesComponent },
+  { path: 'directivas', component: DirectivasComponent }
 ];
 
 @NgModule({
